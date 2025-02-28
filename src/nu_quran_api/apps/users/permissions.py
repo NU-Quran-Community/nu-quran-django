75be3a51fcec4037b7725ddd29afeba075d964b4
Youssef Fathy <32334265+0xzer0x@users.noreply.github.com>
2025-02-28 07:41:36 +0200

    fix: incorrect permission check for user modification

--- src/nu_quran_api/apps/users/permissions.py
+++ src/nu_quran_api/apps/users/permissions.py
@@ -9,18 +9,15 @@ class CanModifyUser(BasePermission):
     def has_object_permission(
         self, request: Request, view: APIView, obj: models.User
     ) -> bool:
-        if request.user and request.user.has_perm("users.change_user"):
-            # NOTE: only admins can edit the following fields of a user:
-            # - referrer
-            # - supervisor
-            # - groups
-            if any(
-                field in request.data for field in ("referrer", "supervisor", "groups")
-            ):
-                return request.user.groups.filter(name="Admin").exists()
-            else:
-                return True
-        return obj == request.user
+        if not request.user:
+            return False
+        # NOTE: only admins can edit the following fields of a user:
+        # - referrer
+        # - supervisor
+        # - groups
+        if any(field in request.data for field in ("referrer", "supervisor", "groups")):
+            return request.user.groups.filter(name="Admin").exists()
+        return obj == request.user or request.user.has_perm("users.change_user")
 
 
 class CanDeleteUser(BasePermission):
