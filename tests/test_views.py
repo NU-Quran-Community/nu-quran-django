232129a6789755dd94e8927432bfa58d92f139a2
Mo-UNO <mohamedamir4491@gmail.com>
2025-03-21 15:41:42 +0200

    test: added tests to get user points

--- tests/test_views.py
+++ tests/test_views.py
@@ -149,3 +149,48 @@ class TestAdminActivityPermissions:
         client.credentials(HTTP_AUTHORIZATION=f"Bearer {jwt_admin_token}")
         response = client.delete(f"/users/{activity.user.id}/activities/{activity.id}/")
         assert response.status_code == status.HTTP_204_NO_CONTENT
+
+
+@pytest.mark.django_db
+class TestUserPointsAPI:
+    def test_get_all_users_points(
+        self, client: APIClient, existing_user, admin_user, activity, jwt_admin_token
+    ):
+        client.credentials(HTTP_AUTHORIZATION=f"Bearer {jwt_admin_token}")
+        response = client.get("/users/points/")
+        expected_data = {
+            "count": 2,
+            "next": None,
+            "previous": None,
+            "results": [
+                {"user": existing_user.id, "points": 1, "activities": [activity.id]},
+                {"user": admin_user.id, "points": 0, "activities": []},
+            ],
+        }
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data == expected_data
+
+    def test_get_user_points_by_id(
+        self, client: APIClient, jwt_admin_token, existing_user, activity
+    ):
+        client.credentials(HTTP_AUTHORIZATION=f"Bearer {jwt_admin_token}")
+        response: Response = client.get(f"/users/{existing_user.id}/points/")
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data["user"] == existing_user.id
+        assert "points" in response.data
+        assert "activities" in response.data
+
+    def test_get_user_points_no_activities(
+        self, client: APIClient, jwt_admin_token, existing_user
+    ):
+        client.credentials(HTTP_AUTHORIZATION=f"Bearer {jwt_admin_token}")
+        response: Response = client.get(f"/users/{existing_user.id}/points/")
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data["user"] == existing_user.id
+        assert response.data["points"] == 0
+        assert list(response.data["activities"]) == []
+
+    def test_get_nonexistent_user_points(self, client: APIClient, jwt_admin_token):
+        client.credentials(HTTP_AUTHORIZATION=f"Bearer {jwt_admin_token}")
+        response: Response = client.get("/users/999999/points/")
+        assert response.status_code == status.HTTP_404_NOT_FOUND
