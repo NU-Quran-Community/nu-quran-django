6235c927d559a2cd102ac47e782ec3e61d6e25f5
Youssef <Y.Ehab2289@nu.edu.eg>
2025-05-21 02:03:28 +0300

    feat : create Goal model and migration

--- src/nu_quran_api/apps/goals/models.py
+++ src/nu_quran_api/apps/goals/models.py
@@ -1,3 +1,21 @@
-# from django.db import models
+from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+
+class Goal(models.Model):
+    class Meta:
+        ordering = ["created_at"]
+        verbose_name_plural = "Goals"
+
+    class Scope(models.TextChoices):
+        MONTHLY = "monthly", "Monthly"
+        YEARLY = "yearly", "Yearly"
+
+    scope = models.CharField(
+        max_length=10, choices=Scope.choices, default=Scope.MONTHLY
+    )
+    title = models.CharField(max_length=255, blank=False, null=False)
+    description = models.CharField(max_length=255, blank=True, null=True)
+    target = models.IntegerField(validators=[MinValueValidator(0)])
+    current = models.IntegerField(validators=[MinValueValidator(0)])
+    created_at = models.DateTimeField(auto_now_add=True)
