1d3f78637cd3bda9f820c1465a3fff624e6eb610
Youssef <Y.Ehab2289@nu.edu.eg>
2025-06-08 23:35:10 +0300

    feat: create GoalsViewSet with relevant permission handling for each action

--- src/nu_quran_api/apps/goals/views.py
+++ src/nu_quran_api/apps/goals/views.py
@@ -1,1 +1,21 @@
-# Create your views here.
+from rest_framework import viewsets, permissions
+from .serializers import GoalSerializer
+from .permissions import CanCreateGoal, CanDeleteGoal, CanModifyGoal
+from .models import Goal
+
+
+class GoalViewSet(viewsets.ModelViewSet):
+    serializer_class = GoalSerializer
+    queryset = Goal.objects.all()
+
+    def get_permissions(self):
+        permission_classes: list[permissions.BasePermission] = []
+        if self.action in ("list", "retrieve"):
+            permission_classes = [permissions.IsAuthenticated]
+        elif self.action == "create":
+            permission_classes = [CanCreateGoal]
+        elif self.action in ("update", "partial_update"):
+            permission_classes = [CanModifyGoal]
+        elif self.action == "delete":
+            permission_classes = [CanDeleteGoal]
+        return [permission() for permission in permission_classes]
